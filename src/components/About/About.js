09755d268c4135fa710ca134be6eaf6e7635e746
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-08-10 03:02:30 -0400

    WIP

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,26 @@
-// src/components/About.jsx
 import React from 'react';
+import { Parallax } from 'react-parallax';
 import './About.css';
 
 const About = () => (
-  <section id="about" className="about" data-aos="fade-up">
-    <h2 className="about-title">About Me</h2>
-    <p className="about-description">
-      I'm a software developer with experience in React, DevOps, and data analytics. I enjoy solving challenging problems and working on innovative projects. My goal is to create solutions that have a real-world impact.
-    </p>
-  </section>
+  <Parallax bgImage="/About.jpg" strength={500}>
+    <section id="about" className="about animated-background" style={{ backgroundImage: 'url(/About.jpg)' }}>
+      <div className="about-content" data-aos="fade-up">
+        <h2 className="about-title">About Me</h2>
+        <div className="about-description">
+          <p>
+            A dedicated and proven software developer and DevOps engineer with a strong foundation in Computer Science and Statistics
+          </p>
+          <p>
+            Passionate about problem-solving and tackling challenging tasks, Aiming to leverage and enhance my software development and data analytics skills to address real-world and business problems
+          </p>
+          <p>
+            Always looking to expand my knowledge, especially in AI, Machine Learning, and Data Science to drive innovative solutions and continuous improvement
+          </p>
+        </div>
+      </div>
+    </section>
+  </Parallax>
 );
 
 export default About;
