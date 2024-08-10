f4aaaff98699dabd231a668c44a19ffab3c23a93
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-08-10 14:45:54 -0400

    education

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,13 +2,6 @@ import React from 'react';
 import { Parallax } from 'react-parallax';
 import './About.css';
 
-const Skill = ({ img, title }) => (
-  <div className="about-skill" data-aos="fade-up">
-    <img src={img} alt={title} title={title} />
-    <div className="tooltip">{title}</div>
-  </div>
-);
-
 const About = () => (
   <Parallax bgImage="/About.jpg" strength={700}>
     <section id="about" className="about full-height">
@@ -24,6 +17,33 @@ const About = () => (
           <p className='about-slide-right'>
             I'm always looking to expand my knowledge, especially in AI, Machine Learning, and Data Science to drive innovative solutions and continuous improvement
           </p>
+        </div>
+        <div className="about-education">
+          <h3>Education</h3>
+          <div className="about-education-item">
+            <div className='education-item-title'>
+              <img src='/mcgill-logo.png' alt='McGill University' />
+              <h4>Bachelor of Science in Computer Science and Statistics</h4>
+            </div>
+            <div className="about-education-item-content">
+              <p>
+                McGill University, Montreal, QC, Canada
+              </p>
+              <div className='education-date'> September 2018 - May 2023</div>
+            </div>
+          </div>
+          <div className="about-education-item">
+            <div className='education-item-title'>
+              <img src='/clw.png' alt='McGill University' />
+              <h4>Scientific Baccaleaureat with specialty in Math and Physics</h4>
+            </div>
+            <div className="about-education-item-content">
+              <p>
+                College Louise Wegmann, Beirut, Lebanon
+              </p>
+              <div className='education-date'> September 2015 - June 2018</div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
