f93afa4e0351d1556b1016d922c878478c8a29a8
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-08-11 02:15:29 -0400

    projects ui fix

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -17,10 +17,12 @@ const Projects = () => (
               <h3>Blind Face Restoration</h3>
             </div>
             <div className="project-card-back">
-              <p><a href='https://openreview.net/group?id=ML_Reproducibility_Challenge/2021/Fall'> Reproducibility challenge </a></p>
-              <p>Based on: <a href='https://arxiv.org/pdf/2101.04061.pdf'>Towards Real-World Blind Face Restoration with Generative Facial Prior</a></p>
-              <p>Restored old or blurry images using generative adversarial networks</p>
-              <p> <a href='https://github.com/wassimwazzi/Image-Restoration'>(GitHub)</a></p>
+              <div className="inner-content">
+                <p><a href='https://openreview.net/group?id=ML_Reproducibility_Challenge/2021/Fall'> Reproducibility challenge </a></p>
+                <p>Based on: <a href='https://arxiv.org/pdf/2101.04061.pdf'>Towards Real-World Blind Face Restoration with Generative Facial Prior</a></p>
+                <p>Restored old or blurry images using generative adversarial networks</p>
+                <p> <a href='https://github.com/wassimwazzi/Image-Restoration'>(GitHub)</a></p>
+              </div>
             </div>
           </div>
         </div>
@@ -30,9 +32,11 @@ const Projects = () => (
               <h3>Personal Budgeting App </h3>
             </div>
             <div className="project-card-back">
-              <p>Financial goals planning and detailed budgeting, showing spend per category and summary dashboard</p>
-              <p>Automated transaction retrieval from bank and categorization</p>
-              <p> <a href='https://github.com/wassimwazzi/budget-manager-web'>(GitHub)</a></p>
+              <div className="inner-content">
+                <p>Financial goals planning and detailed budgeting, showing spend per category and summary dashboard</p>
+                <p>Automated transaction retrieval from bank and categorization</p>
+                <p> <a href='https://github.com/wassimwazzi/budget-manager-web'>(GitHub)</a></p>
+              </div>
             </div>
           </div>
         </div>
@@ -42,9 +46,11 @@ const Projects = () => (
               <h3>Multi-label classification of handwritten digits and English alphabet</h3>
             </div>
             <div className="project-card-back">
-              <p> Built a neural network to correctly classify hand-written numbers and letters on images </p>
-              <p>Achieved over 95% accuracy on over 40, 000 test images</p>
-              <p> <a href='https://github.com/wassimwazzi/Multi-Label-Classification'>(GitHub)</a></p>
+              <div className="inner-content">
+                <p> Built a neural network to correctly classify hand-written numbers and letters on images </p>
+                <p>Achieved over 95% accuracy on over 40, 000 test images</p>
+                <p> <a href='https://github.com/wassimwazzi/Multi-Label-Classification'>(GitHub)</a></p>
+              </div>
             </div>
           </div>
         </div>
@@ -54,9 +60,11 @@ const Projects = () => (
               <h3>Chess game and AI </h3>
             </div>
             <div className="project-card-back">
-              <p>Designed a chess engine in python to support 2 human or AI players, with graphics</p>
-              <p>Created multiple AI players using minimax and Monte Carlo Algorithm</p>
-              <p> <a href='https://github.com/wassimwazzi/Chess'>(GitHub)</a></p>
+              <div className="inner-content">
+                <p>Designed a chess engine in python to support 2 human or AI players, with graphics</p>
+                <p>Created multiple AI players using minimax and Monte Carlo Algorithm</p>
+                <p> <a href='https://github.com/wassimwazzi/Chess'>(GitHub)</a></p>
+              </div>
             </div>
           </div>
         </div>
@@ -66,10 +74,12 @@ const Projects = () => (
               <h3>Splendor Game </h3>
             </div>
             <div className="project-card-back">
-              <p>Collaborated on a team of 6 to develop the Splendor game with an expansion pack for the  software engineering final project</p>
-              <p>The project featured a Lobby Service for online multiplayer support, a Python-based UI for an intuitive user interface, and a robust Java backend for seamless game logic</p>
-              <p>Dockerized the application, enabling continuous integration and deployment</p>
-              <p> <a href='https://github.com/wassimwazzi/splendor'>(GitHub)</a></p>
+              <div className="inner-content">
+                <p>Collaborated on a team of 6 to develop the Splendor game with an expansion pack for the  software engineering final project</p>
+                <p>The project featured a Lobby Service for online multiplayer support, a Python-based UI for an intuitive user interface, and a robust Java backend for seamless game logic</p>
+                <p>Dockerized the application, enabling continuous integration and deployment</p>
+                <p> <a href='https://github.com/wassimwazzi/splendor'>(GitHub)</a></p>
+              </div>
             </div>
           </div>
         </div>
