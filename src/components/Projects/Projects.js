0af528dc2b38ee6da5a0aa830a4412d6665ddcb5
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-10-09 22:54:49 -0400

    style

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Parallax } from 'react-parallax';
 import './Projects.css';
 
+const LinkProj = ({ link }) => (
+  <div className='project-link'>
+    <img src='./github.png' style={{ width: '50px' }} alt='github'/>
+    <a href={link}> Check it out!</a>
+  </div>
+)
+
 const Projects = () => (
   <Parallax
     bgImage="/Projects.jpg"
@@ -12,81 +19,76 @@ const Projects = () => (
       <h2 className="projects-title">Projects</h2>
       <div className="projects-list">
         <div className="project-card" data-aos="zoom-in">
-          <a href='https://github.com/wassimwazzi/Image-Restoration'>
-            <div className="project-card-inner">
-              <div className="project-card-front">
-                <h3>Blind Face Restoration</h3>
-              </div>
-              <div className="project-card-back">
-                <div className="inner-content">
-                  <p><a href='https://openreview.net/group?id=ML_Reproducibility_Challenge/2021/Fall'> Reproducibility challenge </a></p>
-                  <p>Based on: <a href='https://arxiv.org/pdf/2101.04061.pdf'>Towards Real-World Blind Face Restoration with Generative Facial Prior</a></p>
-                  <p>Restored old or blurry images using generative adversarial networks</p>
-                </div>
+          <div className="project-card-inner">
+            <div className="project-card-front">
+              <h3>Blind Face Restoration</h3>
+            </div>
+            <div className="project-card-back">
+              <div className="inner-content">
+                <p><a href='https://openreview.net/group?id=ML_Reproducibility_Challenge/2021/Fall'> Reproducibility challenge </a></p>
+                <p>Based on: <a href='https://arxiv.org/pdf/2101.04061.pdf'>Towards Real-World Blind Face Restoration with Generative Facial Prior</a></p>
+                <p>Restored old or blurry images using generative adversarial networks</p>
+                <LinkProj link='https://github.com/wassimwazzi/Image-Restoration' />
               </div>
             </div>
-          </a>
+          </div>
         </div>
         <div className="project-card" data-aos="zoom-in">
-          <a href='https://github.com/wassimwazzi/budget-manager-web'>
-            <div className="project-card-inner">
-              <div className="project-card-front">
-                <h3>Personal Budgeting App </h3>
-              </div>
-              <div className="project-card-back">
-                <div className="inner-content">
-                  <p>Financial goals planning and detailed budgeting, showing spend per category and summary dashboard</p>
-                  <p>Automated transaction retrieval from bank and categorization</p>
-                </div>
+          <div className="project-card-inner">
+            <div className="project-card-front">
+              <h3>Personal Budgeting App </h3>
+            </div>
+            <div className="project-card-back">
+              <div className="inner-content">
+                <p>Financial goals planning and detailed budgeting, showing spend per category and summary dashboard</p>
+                <p>Automated transaction retrieval from bank and categorization</p>
+                <LinkProj link='https://github.com/wassimwazzi/budget-manager-web' />
               </div>
             </div>
-          </a>
+          </div>
         </div>
         <div className="project-card" data-aos="zoom-in">
-          <a href='https://github.com/wassimwazzi/Multi-Label-Classification'>
-            <div className="project-card-inner">
-              <div className="project-card-front">
-                <h3>Multi-label classification of handwritten digits and English alphabet</h3>
-              </div>
-              <div className="project-card-back">
-                <div className="inner-content">
-                  <p> Built a neural network to correctly classify hand-written numbers and letters on images </p>
-                  <p>Achieved over 95% accuracy on over 40, 000 test images</p>
-                </div>
+          <div className="project-card-inner">
+            <div className="project-card-front">
+              <h3>Multi-label classification of handwritten digits and English alphabet</h3>
+            </div>
+            <div className="project-card-back">
+              <div className="inner-content">
+                <p> Built a neural network to correctly classify hand-written numbers and letters on images </p>
+                <p>Achieved over 95% accuracy on over 40, 000 test images</p>
+                <LinkProj link='https://github.com/wassimwazzi/Multi-Label-Classification' />
               </div>
             </div>
-          </a>
+          </div>
         </div>
         <div className="project-card" data-aos="zoom-in">
-          <a href='https://github.com/wassimwazzi/Chess'>
-            <div className="project-card-inner">
-              <div className="project-card-front">
-                <h3>Chess game and AI </h3>
-              </div>
-              <div className="project-card-back">
-                <div className="inner-content">
-                  <p>Designed a chess engine in python to support 2 human or AI players, with graphics</p>
-                  <p>Created multiple AI players using minimax and Monte Carlo Algorithm</p>
-                </div>
+          <div className="project-card-inner">
+            <div className="project-card-front">
+              <h3>Chess game and AI </h3>
+            </div>
+            <div className="project-card-back">
+              <div className="inner-content">
+                <p>Designed a chess engine in python to support 2 human or AI players, with graphics</p>
+                <p>Created multiple AI players using minimax and Monte Carlo Algorithm</p>
+                <LinkProj link='https://github.com/wassimwazzi/Chess' />
               </div>
             </div>
-          </a>
+          </div>
         </div>
         <div className="project-card" data-aos="zoom-in">
-          <a href='https://github.com/wassimwazzi/splendor'>
-            <div className="project-card-inner">
-              <div className="project-card-front">
-                <h3>Splendor Game </h3>
-              </div>
-              <div className="project-card-back">
-                <div className="inner-content">
-                  <p>Collaborated on a team of 6 to develop the Splendor game with an expansion pack for the  software engineering final project</p>
-                  <p>The project featured a Lobby Service for online multiplayer support, a Python-based UI for an intuitive user interface, and a robust Java backend for seamless game logic</p>
-                  <p>Dockerized the application, enabling continuous integration and deployment</p>
-                </div>
+          <div className="project-card-inner">
+            <div className="project-card-front">
+              <h3>Splendor Game </h3>
+            </div>
+            <div className="project-card-back">
+              <div className="inner-content">
+                <p>Collaborated on a team of 6 to develop the Splendor game with an expansion pack for the  software engineering final project</p>
+                <p>The project featured a Lobby Service for online multiplayer support, a Python-based UI for an intuitive user interface, and a robust Java backend for seamless game logic</p>
+                <p>Dockerized the application, enabling continuous integration and deployment</p>
+                <LinkProj link='https://github.com/wassimwazzi/splendor' />
               </div>
             </div>
-          </a>
+          </div>
         </div>
       </div>
     </section>
