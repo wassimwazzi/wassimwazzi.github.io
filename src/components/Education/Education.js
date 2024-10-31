f65b2491683cb17fc00af37185bf48f9849aa558
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-10-30 20:10:15 -0400

    WIP - education

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -8,41 +8,33 @@ const EDUCATION_ITEMS = [
     institution: 'McGill University, Montreal, QC, Canada',
     date: 'September 2018 - May 2023',
     logo: '/mcgill-logo.png',
+    description: 'Focus on software development, data science, and statistical analysis. Completed notable projects in machine learning and data analytics.',
   },
   {
     title: 'Scientific Baccalaureate with Specialty in Math and Physics',
     institution: 'College Louise Wegmann, Beirut, Lebanon',
     date: 'September 2015 - June 2018',
     logo: '/clw.png',
-  },
-  {
-    title: 'Bachelor of Science in Computer Science and Statistics',
-    institution: 'McGill University, Montreal, QC, Canada',
-    date: 'September 2018 - May 2023',
-    logo: '/mcgill-logo.png',
-  },
-  {
-    title: 'Bachelor of Science in Computer Science and Statistics',
-    institution: 'McGill University, Montreal, QC, Canada',
-    date: 'September 2018 - May 2023',
-    logo: '/mcgill-logo.png',
+    description: 'Graduated with honors, specializing in advanced mathematics and physics.',
   },
 ];
+
 const Education = () => {
-
-
   return (
     <section id="education" className="education">
-      <Scrollable title={'Education'}>
+      <Scrollable>
         {EDUCATION_ITEMS.map((item, index) => (
           <div className="education-item" key={index}>
-            <div className="education-item-title">
-              <img src={item.logo} alt={item.institution} className="education-logo" />
-              <div>{item.title}</div>
-            </div>
-            <div className="education-item-content">
-              <p>{item.institution}</p>
-              <div className="education-date">{item.date}</div>
+            <img src={item.logo} alt={item.institution} className="education-logo" />
+            <div className='education-item-content'>
+              <div className="education-item-title">
+                {item.title}
+              </div>
+              <div className="education-item-instirtution">
+                <p>{item.institution}</p>
+                <div className="education-date">{item.date}</div>
+              </div>
+              <p className="education-description">{item.description}</p>
             </div>
           </div>
         ))}
